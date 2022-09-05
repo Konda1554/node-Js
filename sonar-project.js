e80853bd50e3a9e37e684f6e8014b7bf8423b26a
Konda1554 <106516329+Konda1554@users.noreply.github.com>
2022-09-05 10:06:38 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.201.227.174:9000/',
+  serverUrl: 'http://3.83.40.3:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
